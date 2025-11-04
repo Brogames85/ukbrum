dc322cad545164623d059645df0b9e30ab0a03f7
Mahir Handa <78787711+Brogames85@users.noreply.github.com>
2025-11-04 19:18:30 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -225,7 +225,7 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
-        "backend.server:app",
+        "server:app",
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 10000)),
         reload=False
